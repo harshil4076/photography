fedef7defc0d8aadf78466af901f344d1e083aba
Harshil Patel <harshil4076@gmail.com>
2020-02-11 16:54:38 -0500

    www

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var http = require('http').createServer(app);
 const cors = require("cors")
 const path = require('path');
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const adminRoutes = require('./routes/admin/admin');
-const Category = require('./models/category')
+// const mongoose = require("mongoose");
+// const adminRoutes = require('./routes/admin/admin');
+// const Category = require('./models/category')
 const io = require('socket.io')(http);
 
 // const url = process.env.DATABASEURL || "mongodb://localhost:27017/hk_photo" 
@@ -20,17 +20,17 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
-app.use(adminRoutes);
+// app.use(adminRoutes);
 // getting all category
-app.get("/api/allcategory", async (req, res) => {
-	try{
-		let allcategory = await Category.find();
-		// let { category } = allcategory;
-		res.status(200).json(allcategory);
-	}catch(err){
-		res.send(err);
-	}
-})
+// app.get("/api/allcategory", async (req, res) => {
+// 	try{
+// 		let allcategory = await Category.find();
+// 		// let { category } = allcategory;
+// 		res.status(200).json(allcategory);
+// 	}catch(err){
+// 		res.send(err);
+// 	}
+// })
 
 io.on('connection', function(socket){
 	console.log('a user connected');
